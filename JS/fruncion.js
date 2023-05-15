acd2c93b1df9c3ab40fdc4c9a06158a6248a3618
Fausto Gallego <faustogallego666@gmail.com>
2023-05-15 17:04:37 -0300

    ,
    ,

--- JS/fruncion.js
+++ JS/fruncion.js
@@ -1,6 +1,8 @@
 function fruncion(params){
     let input = document.querySelector("#entrada");
     let p = document.querySelector("#salida");
+    let divIMG = document.querySelector ("#img");
+    divIMG.innerHTML = "<img src='../IMG/isac.png' alt='llorin'>"
     let valor = input.value;
     p.innerText = valor;
     console.log(input);
