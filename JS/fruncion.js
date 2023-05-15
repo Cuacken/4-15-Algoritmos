76d0ca620feece2d3a8c6990a71e31d6bd0f5cc5
Fausto Gallego <faustogallego666@gmail.com>
2023-05-15 16:44:06 -0300

    ys

--- JS/fruncion.js
+++ JS/fruncion.js
@@ -1,6 +1,10 @@
-function fruncion(){
+function fruncion(params){
     let input = document.querySelector("#entrada");
+    let p = document.querySelector("#salida");
     let valor = input.value;
+    p.innerText = valor;
     console.log(input);
-    console.log(valor)
+    console.dir(input);
+    console.log(p.value);
+    console.log(valor);
 }
